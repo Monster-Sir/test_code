4dbeeb8d6f5c0011d7934306116edb3f7f7ba944
lizhaojun-ubuntu22.04 <lizhaojun97@qq.com>
2023-09-14 19:43:43 +0800

    P12 git init

---
+++ code/test1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello test1.c\n");
+	printf("this diff command test.\n");
+	printf("this diff command test 2.\n");
+	printf("this diff --cached test 1.\n");
+	printf("this diff --cached test in workspace.\n");
+	return 0;
+}
---
+++ code/test2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello\n");
+	printf("this is command diff for test tow file difference");
+	return 0;
+}
